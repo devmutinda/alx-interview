deb03e6933ad4306ba93d7dbdeb01bf4ab79fa07
devmutinda <davidkymutinda@gmail.com>
2022-12-23 13:44:54 +0000

    Made changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,4 +4,21 @@ This module returns a list of lists of integers representing the Pascal's triang
 """
 
 def pascal_triangle(n):
-    
+    """Returns list of lists"""    
+    if n <= 0:
+        return []
+    new_list = []
+    for i in range(n):
+        col = []
+
+        for k in range(i + 1):
+            if not k or k == i:
+                col.append(1)
+
+            else:
+                col.append(new_list[i - 1][k] + new_list[i - 1][k - 1])
+
+        new_list.append(col)
+
+    return new_list
+        
